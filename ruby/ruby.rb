d78a96ba0620dca81a76b5bb8e71a3bbb66aaf35
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2023-11-10 19:27:35 -0500

    adlibs

--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -6,10 +6,21 @@
 
 # puts "Hey #{name}, you are #{age} years old!"
 
-puts "Enter a number: "
-num1 = gets.chomp().to_f
+# puts "Enter a number: "
+# num1 = gets.chomp().to_f
 
-puts "Enter another number: "
-num2 = gets.chomp().to_f
+# puts "Enter another number: "
+# num2 = gets.chomp().to_f
 
-puts "#{num1} plus #{num2} eqauls #{num1 + num2}"
+# puts "#{num1} plus #{num2} eqauls #{num1 + num2}"
+
+# puts "Give a color: "
+# color = gets.chomp()
+# puts "Give a plural noun: "
+# plural_noun = gets.chomp()
+# puts "Give a Celebrity: "
+# celebrity = gets.chomp()
+
+# puts "Roses are #{color.downcase}"
+# puts "#{plural_noun.capitalize} are blue"
+# puts "I love #{celebrity.capitalize}"
