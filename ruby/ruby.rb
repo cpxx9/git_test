647a0f6807c88903d064f754c7c25a3e42746238
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2023-11-10 21:34:39 -0500

    more If statements

--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -97,3 +97,15 @@
 # else
 #   puts "geez"
 # end
+
+def find_max(num1, num2, num3)
+  if num1 >= num2 and num1 >= num3
+    return num1
+  elsif num2 >= num1 and num2 >= num3
+    return num2
+  else
+    return num3
+  end
+end
+
+puts find_max(1, 29, 3)
