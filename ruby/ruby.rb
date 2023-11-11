154a12af0867e830d50424b9ac5d3677c77323fb
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2023-11-10 19:19:23 -0500

    user input

--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -1,7 +1,15 @@
-puts "Enter your name: "
-name = gets.chomp()
+# puts "Enter your name: "
+# name = gets.chomp()
 
-puts "Enter your age: "
-age = gets.chomp()
+# puts "Enter your age: "
+# age = gets.chomp()
 
-puts "Hey #{name}, you are #{age} years old!"
+# puts "Hey #{name}, you are #{age} years old!"
+
+puts "Enter a number: "
+num1 = gets.chomp().to_f
+
+puts "Enter another number: "
+num2 = gets.chomp().to_f
+
+puts "#{num1} plus #{num2} eqauls #{num1 + num2}"
