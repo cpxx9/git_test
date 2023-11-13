182a31c8aad5044bfe2ff07c2ec55bc53f7b31d4
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2023-11-13 10:00:09 -0500

    while loops

--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -140,8 +140,8 @@
 
 #WHILE LOOPS
 #___________________________________________________________________________
-index = 1
-while index <= 5
-  puts index
-  index += 1
-end
+# index = 1
+# while index <= 5
+#   puts index
+#   index += 1
+# end
