7cd2e97445b7db39a08f51daba507e63e085f2be
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2023-11-13 09:44:38 -0500

    case expression

--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -112,28 +112,28 @@
 
 #CASE EXPRESSION
 #___________________________________________________________________________
-def get_day_name(day)
-  day_name = ""
-  case day
-    when "mon"
-      day_name = "Monday"
-    when "tue"
-      day_name = "Tuesday"
-    when "wed"
-      day_name = "Wednesday"
-    when "thu"
-      day_name = "Thursday"
-    when "fri"
-      day_name = "Friday"
-    when "sat"
-      day_name = "Satuday"
-    when "sun"
-      day_name = "Sunday"
-    else
-      day_name = "Invalid Abbrevation"
-  end
+# def get_day_name(day)
+#   day_name = ""
+#   case day
+#     when "mon"
+#       day_name = "Monday"
+#     when "tue"
+#       day_name = "Tuesday"
+#     when "wed"
+#       day_name = "Wednesday"
+#     when "thu"
+#       day_name = "Thursday"
+#     when "fri"
+#       day_name = "Friday"
+#     when "sat"
+#       day_name = "Satuday"
+#     when "sun"
+#       day_name = "Sunday"
+#     else
+#       day_name = "Invalid Abbrevation"
+#   end
 
-  return day_name
-end
+#   return day_name
+# end
 
-puts get_day_name("sat")
+# puts get_day_name("sat")
