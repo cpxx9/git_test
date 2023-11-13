da44c4aef6a574f60654b18ed5ea198fe05e8140
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2023-11-13 15:05:52 -0500

    reading files

--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -221,3 +221,17 @@ comments
 
 #READING FILES
 #___________________________________________________________________________
+# File.open("employees.txt", "r") do |file|
+#   # puts file.readline()
+#   # puts file.readline()
+#   # puts file.readchar()
+#   for line in file.readlines()
+#     puts line.upcase
+#   end
+# end
+
+file = File.open("employees.txt", "r")
+
+puts file.read
+
+file.close()
