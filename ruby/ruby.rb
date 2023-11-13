0193011ed74a1e2259f8cd366b6ce7f60aabdb3b
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2023-11-13 14:34:03 -0500

    exponent method

--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -197,3 +197,16 @@
 # # 6.times do |index|
 # #   puts index
 # # end
+
+#EXPONENT METHOD
+#___________________________________________________________________________
+
+# def pow(base, power)
+#   result = 1
+#   power.times do
+#     result = result * base
+#   end
+#   return result
+# end
+
+# puts pow(-5, 4)
