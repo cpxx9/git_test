2c94f5a47a1ce4430d95f31c4c99d94e225b26b9
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2023-11-13 20:20:42 -0500

    basic quiz

--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -332,3 +332,38 @@ comments
 # puts student2.has_honors
 # puts student3.has_honors
 # puts student4.has_honors
+
+#CREATING A QUIZ
+#___________________________________________________________________________
+class Question
+  attr_accessor :prompt, :answer
+  def initialize(prompt, answer)
+    @prompt = prompt
+    @answer = answer
+  end
+end
+
+p1 = "What color are apples?\n(a)red\n(b)purple\n(c)blue"
+p2 = "What color are bananas?\n(a)pink\n(b)red\n(c)yellow"
+p3 = "What color are pears?\n(a)red\n(b)green\n(c)blue"
+
+questions = [
+  Question.new(p1, "a"),
+  Question.new(p2, "c"),
+  Question.new(p3, "b")
+]
+
+def run_test(questions)
+  answer = ""
+  score = 0
+  for question in questions
+    puts question.prompt
+    answer = gets.chomp()
+    if answer == question.answer
+      score += 1
+    end
+  end
+  puts "You got #{score} out of #{questions.length} right."
+end
+
+run_test(questions)
