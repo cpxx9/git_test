46c4575b1f7b26830ba1ee5a7b04885f4f945711
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-01-16 18:06:25 -0500

    reset files for new module

---
+++ JS/scripts.js
@@ -0,0 +1,13 @@
+setInterval(loadingDots, 500);
+      function loadingDots() {
+        const title = document.querySelector("#title");
+        if (title.textContent === "Testing") {
+          title.textContent = "Testing.";
+        } else if (title.textContent === "Testing.") {
+          title.textContent = "Testing..";
+        } else if (title.textContent === "Testing..") {
+          title.textContent = "Testing...";
+        } else if (title.textContent === "Testing...") {
+          title.textContent = "Testing";
+        }
+      }
