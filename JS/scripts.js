12fe472375327510da82f1f3b36879b8a70fee86
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-01-27 12:52:37 -0500

    obnject function

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,14 +1,9 @@
-setInterval(loadingDots, 500);
-
-function loadingDots() {
-  const title = document.querySelector("#title");
-  if (title.textContent === "Testing") {
-    title.textContent = "Testing.";
-  } else if (title.textContent === "Testing.") {
-    title.textContent = "Testing..";
-  } else if (title.textContent === "Testing..") {
-    title.textContent = "Testing...";
-  } else if (title.textContent === "Testing...") {
-    title.textContent = "Testing";
-  }
+function Book(title, author, pages, hasRead) {
+  this.title = title;  
+  this.author = author;  
+  this.pages = pages;  
+  this.hasRead = hasRead;
+  this.info = function() {
+    return `${this.title.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())} by ${this.author.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())}, ${this.pages} pages, ${this.hasRead ? "has been read" : "not read yet"}`;
+  };
 }
