9f79e6ba8ce30f7a0306948d8893e28c1b197290
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-01-27 14:23:25 -0500

    created object prototype

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,13 +1,29 @@
-function Book(title, author, pages, hasRead) {
-  this.title = title;  
-  this.author = author;  
-  this.pages = pages;  
-  this.hasRead = hasRead;
-  this.info = function() {
-    return `${this.title.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())} by ${this.author.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())}, ${this.pages} pages, ${this.hasRead ? "has been read" : "not read yet"}`;
+// function Book(title, author, pages, hasRead) {
+//   this.title = title;  
+//   this.author = author;  
+//   this.pages = pages;  
+//   this.hasRead = hasRead;
+//   this.info = function() {
+//     return `${this.title.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())} by ${this.author.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())}, ${this.pages} pages, ${this.hasRead ? "has been read" : "not read yet"}`;
+//   };
+// }
+
+// const theHobbit = new Book("the hobbit", "j.R.R. tolkien", 295, false);
+
+// console.log(theHobbit.info());
+
+function Player(name, marker) {
+  this.name = name;
+  this.marker = marker;
+  this.sayName = function() {
+    console.log(this.name)
   };
 }
 
-const theHobbit = new Book("the hobbit", "j.R.R. tolkien", 295, false);
+Player.prototype.sayHello = function () {
+  console.log(`Hello, I'm a player`);
+};
 
-console.log(theHobbit.info());
+const player1 = new Player('steve', 'X');
+const player2 = new Player('also steve', 'O');
+player1.sayHello();
