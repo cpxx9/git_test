a9374af7340021d7b5f309a84e6c2af88b90363e
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-01-28 12:12:30 -0500

    object contructors and prototype inheritance

--- JS/scripts.js
+++ JS/scripts.js
@@ -12,18 +12,52 @@
 
 // console.log(theHobbit.info());
 
-function Player(name, marker) {
-  this.name = name;
-  this.marker = marker;
-  this.sayName = function() {
-    console.log(this.name)
-  };
-}
+// function Player(name, marker) {
+//   this.name = name;
+//   this.marker = marker;
+//   this.sayName = function() {
+//     console.log(this.name)
+//   };
+// }
 
-Player.prototype.sayHello = function () {
-  console.log(`Hello, I'm a player`);
-};
+// Player.prototype.sayHello = function () {
+//   console.log(`Hello, I'm a player`);
+// };
 
-const player1 = new Player('steve', 'X');
-const player2 = new Player('also steve', 'O');
-player1.sayHello();
+// const player1 = new Player('steve', 'X');
+// const player2 = new Player('also steve', 'O');
+// player1.sayHello();
+
+// function Hero(name, level) {
+//   this.name = name;
+//   this.level = level;
+// }
+
+// function Warrior(name, level, weapon) {
+//   Hero.call(this, name, level);
+//   this.weapon = weapon;
+// }
+
+// function Healer(name, level, spell) {
+//   Hero.call(this, name, spell);
+//   this.spell = spell;
+// }
+
+// Object.setPrototypeOf(Warrior.prototype, Hero.prototype);
+// Object.setPrototypeOf(Healer.prototype, Hero.prototype);
+
+// Hero.prototype.greet = function () {
+//   return `${this.name} says hello.`
+// };
+
+// Warrior.prototype.attack = function () {
+//   return `${this.name} attacks with ${this.weapon}.`;
+// };
+
+// Healer.prototype.heal = function () {
+//   return `${this.name} casts ${this.spell}.`;
+// };
+
+// let hero1 = new Warrior('Bjorn', 1, 'axe');
+// let hero2 = new Healer('Kanin', 1, 'cure');
+
