788e5572524d0b832d5c4b22a04f88ee13d0acfa
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-02-07 07:12:18 -0500

    finished modules

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,13 +1,24 @@
-const myModule = {
-  name: 'Will',
-  age: 34,
-  sayName: function() {
-    alert(this.name);
-  },
-  setName: function(newName) {
-    this.name = newName;
-  },
-};
+(function() {
+  const people = {
+    people: ['Will', 'Laura'],
+    init: function() {
+      this.cacheDom();
+      this.render();
+    },
+    cacheDom: function() {
+      this.el = document.querySelector('#peopleModule');
+      this.button = this.el.querySelector('button');
+      this.input = this.el.querySelector('input');
+      this.ul = this.el.querySelector('ul');
+      this.template = this.el.querySelector('#people-template').toString();
+    },
+    render: function() {
+      const data = {
+        people: this.people,
+      };
+      this.ul.html(Mustache.render(this.template, data));
+    },
+  };
 
-myModule.setName('cole');
-myModule.sayName();
+  people.init();
+})()
