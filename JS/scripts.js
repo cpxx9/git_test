6d4793655be0af4de36d22f5231d94400e8d07ec
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-02-21 08:10:38 -0500

    finished mdm classes

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,29 +1,51 @@
-const barker = (state) => ({
-  bark: () => console.log('Woof, I am ' + state.name)
-});
+class Person {
+  static species = 'Homo Sapiens';
 
-const driver = (state) => ({
-  drive: () => state.position = state.position + state.speed
-});
-
-const killer = (state) => ({
-  killer: () => state.name + 'is a killer.'
-});
-
-const murderRobotDog = (name) => {
-  let state = {
-    name,
-    speed: 100,
-    position: 0,
+  static speciesSentance() {
+    return `Humans are classifies as ${this.species}.`
   }
 
-  return Object.assign(
-    {},
-    barker(state),
-    driver(state),
-    killer(state),
-  )
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.hasJob = false;
+  }
+
+  fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  setFirstName(firstName) {
+    this.firstName = firstName;
+  }
+
+  setLastName(lastName) {
+    this.lastName = lastName;
+  }
+
+  set fullName(name) {
+    name = name.split(' ');
+    this.setFirstName(name[0]);
+    this.setLastName(name[1]);
+  }
 }
 
-murderRobotDog('sniffles').bark()
-murderRobotDog('sniffles').drive()
+class Worker extends Person {
+  constructor(firstName, lastName, job) {
+    super(firstName, lastName);
+    this.job = job;
+    this.hasJob = true;
+  }
+
+  setJob(job) {
+    this.job = job;
+  }
+
+  get biography() {
+    const bio = `${this.fullName()} is a ${this.job}.`.toUpperCase();
+    return bio;
+  }
+}
+
+const p1 = new Person("Cole", "Drizzle");
+const w1 = new Worker("Dgad", "Frizzle", "ptute");
