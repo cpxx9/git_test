6849eec7620c099d6bfe0031dfba7d47c6a371a3
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-02-23 09:17:43 -0500

    fixed webpack and npm package on laptop

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,9 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/JS/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  optimization: {
-    runtimeChunk: 'single',
   },
 };
