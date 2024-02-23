ee444704f7afca77828d61991b866c61287b3165
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-02-22 19:42:39 -0500

    added new module

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { loadingDots } from './loading-dots';
 import _ from 'lodash';
+import myName from './myName';
 
 setInterval(loadingDots, 500);
 
 function component() {
   const element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+  element.textContent = myName('Cole');
   return element;
 }
 
