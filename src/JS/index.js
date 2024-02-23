ef386896f67dd696291332fe7b84e5a5c837de13
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-02-23 11:26:56 -0500

    added module for html bundler and edited css

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,6 +1,7 @@
 import { loadingDots } from './loading-dots';
 import _ from 'lodash';
 import myName from './myName';
+import '../CSS/styles.css';
 
 setInterval(loadingDots, 500);
 
@@ -8,6 +9,9 @@ function component() {
   const element = document.createElement('div');
 
   element.textContent = myName('Cole');
+  element.textContent += _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
   return element;
 }
 
