f67786f913db94ebe671cc9568aaa57d98ab75f4
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-02-24 15:43:53 -0500

    added images via webpack

--- src/JS/index.js
+++ src/JS/index.js
@@ -2,6 +2,7 @@ import { loadingDots } from './loading-dots';
 import _ from 'lodash';
 import myName from './myName';
 import '../CSS/styles.css';
+import Odin from '../img/odin.png'
 
 setInterval(loadingDots, 500);
 
@@ -11,6 +12,11 @@ function component() {
   element.textContent = myName('Cole');
   element.textContent += _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  const myIcon = new Image();
+  myIcon.src = Odin;
+
+  element.appendChild(myIcon);
 
   return element;
 }
