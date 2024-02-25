0e438e4a9d5f2102c625e2382446a7c0ae5d24d1
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-02-24 20:53:44 -0500

    fixed dev server conf

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   devtool: 'source-map',
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'dist')
+      directory: path.resolve(__dirname, 'src')
     },
     port: 3000,
     open: true,
@@ -64,4 +64,7 @@ module.exports = {
     // uncomment for space analyzer
     // new BundleAnalyzerPlugin(),
   ],
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };
