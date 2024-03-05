da6018708e271d9ba37685c683be45289d172ed8
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-03-05 09:40:58 -0500

    DOM manipulation with .json data

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    index: path.resolve(__dirname, 'src/JS/index.js'),
+    index: path.resolve(__dirname, './src/JS/index.js'),
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
