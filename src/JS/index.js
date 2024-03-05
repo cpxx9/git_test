b4408ab44c55a05dbe81914632678edcf174c621
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-03-05 09:46:20 -0500

    parse json from string

--- src/JS/index.js
+++ src/JS/index.js
@@ -5,7 +5,8 @@ async function populate() {
   const request = new Request(requestURL);
 
   const response = await fetch(request);
-  const superHeroes = await response.json();
+  const superHeroesText = await response.text();
+  const superHeroes = JSON.parse(superHeroesText);
 
   populateHeader(superHeroes);
   populateHeroes(superHeroes);
