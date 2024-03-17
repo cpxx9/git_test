d1a6e7e7e12cba7c575ed02a000fb19028588b98
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-03-17 11:47:47 -0400

    composition

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,39 +1,45 @@
-class Monster {
-  constructor(name) {
-    this.name = name;
-  }
-
-  attack() {
-    console.log(`${this.name} attacked`);
-  }
-
-  walk() {
-    console.log(`${this.name} walked`);
-  }
+function swimmer({ name }) {
+  return {
+    swim: () => console.log(`${name} swam`),
+  };
 }
 
-class FlyingMonster extends Monster {
-  fly() {
-    console.log(`${this.name} flew`);
-  }
+function flyer({ name }) {
+  return {
+    fly: () => console.log(`${name} flew`),
+  };
 }
 
-class SwimmingMonster extends Monster {
-  swim() {
-    console.log(`${this.name} swam`);
-  }
+function attackerAndWalker({ name }) {
+  return {
+    attack: () => console.log(`${name} attacked`),
+    walk: () => console.log(`${name} walked`),
+  };
 }
 
-const bear = new Monster("bear");
-bear.walk();
-bear.attack();
+function swimmingMonsterCreator(name) {
+  const monster = { name: name };
 
-const eagle = new FlyingMonster("eagle");
-eagle.walk();
-eagle.attack();
-eagle.fly();
+  return {
+    ...monster,
+    ...attackerAndWalker(monster),
+    ...swimmer(monster),
+  };
+}
 
-const shark = new SwimmingMonster("shark");
-shark.walk();
-shark.attack();
-shark.swim();
+function flyingSwimmingMonsterCreator(name) {
+  const monster = { name: name };
+
+  return {
+    ...monster,
+    ...attackerAndWalker(monster),
+    ...swimmer(monster),
+    ...flyer(monster),
+  };
+}
+
+const obj = flyingSwimmingMonsterCreator("Test");
+obj.swim();
+obj.walk();
+obj.attack();
+obj.fly();
