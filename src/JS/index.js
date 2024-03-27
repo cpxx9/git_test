e896c04ebb603d691bbc292df3f3b0d49e641f33
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-03-26 20:57:22 -0400

    added eslint

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,45 +1,9 @@
-function swimmer({ name }) {
-  return {
-    swim: () => console.log(`${name} swam`),
-  };
-}
+const name = 'James';
 
-function flyer({ name }) {
-  return {
-    fly: () => console.log(`${name} flew`),
-  };
-}
+const person = { first: name };
 
-function attackerAndWalker({ name }) {
-  return {
-    attack: () => console.log(`${name} attacked`),
-    walk: () => console.log(`${name} walked`),
-  };
-}
+console.log(person);
 
-function swimmingMonsterCreator(name) {
-  const monster = { name: name };
-
-  return {
-    ...monster,
-    ...attackerAndWalker(monster),
-    ...swimmer(monster),
-  };
-}
-
-function flyingSwimmingMonsterCreator(name) {
-  const monster = { name: name };
-
-  return {
-    ...monster,
-    ...attackerAndWalker(monster),
-    ...swimmer(monster),
-    ...flyer(monster),
-  };
-}
-
-const obj = flyingSwimmingMonsterCreator("Test");
-obj.swim();
-obj.walk();
-obj.attack();
-obj.fly();
+const sayHelloLinting = (fName) => {
+  console.log(`Hello linting, ${fName}`);
+};
