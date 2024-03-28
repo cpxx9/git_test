99914ef3f6b8ce388253d73ed56df8faa9a67055
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-03-28 18:56:31 -0400

    radios change when image changes

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,15 +1,16 @@
 import '../CSS/styles.css';
-const buttons = document.querySelectorAll('[data-carouselBtn]');
+const buttons = document.querySelectorAll('[data-carousel-btn]');
 const slider = document.querySelector('[data-slider]');
 const imageSlides = document.querySelectorAll('.slide');
-console.log(imageSlides);
 
 for (let i = 0; i < imageSlides.length; i++) {
   const slideBtn = document.createElement('input');
   slideBtn.type = 'radio';
   slideBtn.name = 'radio-btn';
   slideBtn.id = `radio${i + 1}`;
-  slideBtn.dataset.sliderBtn = true;
+  slideBtn.classList.add('radio-btn');
+  slideBtn.dataset.radio = `${i}`;
+  if (i === 0) slideBtn.checked = true;
   slider.appendChild(slideBtn);
 }
 
@@ -19,17 +20,23 @@ buttons.forEach((button) => {
     const slides = button
       .closest('[data-carousel]')
       .querySelector('[data-slides]');
-    const sliderBtns = document.querySelectorAll('[data-sliderBtn]');
+    const sliderBtns = slider.children;
 
     const activeSlide = slides.querySelector('[data-active]');
     let newIndex = [...slides.children].indexOf(activeSlide) + offset;
     if (newIndex < 0) newIndex = slides.children.length - 1;
     if (newIndex >= slides.children.length) newIndex = 0;
 
-    console.log(newIndex);
-    console.log(sliderBtns);
     sliderBtns[newIndex].checked = true;
     slides.children[newIndex].dataset.active = true;
     delete activeSlide.dataset.active;
+  });
+});
+
+const radios = document.querySelectorAll('.radio-btn');
+
+radios.forEach((child) => {
+  child.addEventListener('checked', (e) => {
+    console.log(test);
   });
 });
