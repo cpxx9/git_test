5062f1bf47078b54af1b85475da5cdca27bcfaae
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-04-08 13:22:43 -0500

    fixed eslintrc

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,1 +1,24 @@
 import '../CSS/styles.css';
+
+let stock = {
+  fruits: ['strawberry', 'grape', 'banana', 'apple'],
+  liquid: ['water', 'ice'],
+  holder: ['cone', 'cup', 'stick'],
+  toppings: ['chocolate', 'peanuts'],
+};
+
+let isShopOpen = true;
+
+function order(time, work) {
+  return new Promise((resolve, reject) => {
+    if (isShopOpen) {
+      setTimeout(() => {
+        resolve(work());
+      }, time);
+    } else {
+      reject(console.log('Shop is closed'));
+    }
+  });
+}
+
+order(2000, () => console.log(`${stock.fruits[0]} was chosen.`)).then();
