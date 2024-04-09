0b17c97ae398d474a8a8d1e1c964baa42af41563
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-04-09 10:24:22 -0500

    fixed eslint, async promises

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,13 +1,13 @@
 import '../CSS/styles.css';
 
-let stock = {
+const stock = {
   fruits: ['strawberry', 'grape', 'banana', 'apple'],
   liquid: ['water', 'ice'],
   holder: ['cone', 'cup', 'stick'],
   toppings: ['chocolate', 'peanuts'],
 };
 
-let isShopOpen = true;
+const isShopOpen = true;
 
 function order(time, work) {
   return new Promise((resolve, reject) => {
@@ -21,4 +21,33 @@ function order(time, work) {
   });
 }
 
-order(2000, () => console.log(`${stock.fruits[0]} was chosen.`)).then();
+order(2000, () => console.log(`${stock.fruits[0]} was chosen.`))
+  .then(() => order(0, () => console.log('Production has started')))
+  .then(() => order(2000, () => console.log(`The fruit was chopped.`)))
+  .then(() =>
+    order(1000, () => {
+      console.log(`${stock.liquid[0]} and ${stock.liquid[1]} were chosen.`);
+    })
+  )
+  .then(() => order(1000, () => console.log('Started the machine.')))
+  .then(() =>
+    order(2000, () => {
+      console.log(`Ice cream placed on ${stock.holder[0]}.`);
+    })
+  )
+  .then(() =>
+    order(3000, () => {
+      console.log(`${stock.toppings[0]} was selected.`);
+    })
+  )
+  .then(() =>
+    order(2000, () => {
+      console.log('Ice Cream was served.');
+    })
+  )
+  .catch(() => {
+    console.log('Customer left');
+  })
+  .finally(() => {
+    console.log('Day ended, shop closed');
+  });
