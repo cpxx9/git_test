a793327b9aab7e6049b21f52db97519052a62784
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-04-09 14:31:42 -0500

    async java

--- src/JS/index.js
+++ src/JS/index.js
@@ -7,47 +7,48 @@ const stock = {
   toppings: ['chocolate', 'peanuts'],
 };
 
-const isShopOpen = true;
+const isShopOpen = false;
 
-function order(time, work) {
+function time(ms) {
   return new Promise((resolve, reject) => {
     if (isShopOpen) {
-      setTimeout(() => {
-        resolve(work());
-      }, time);
+      setTimeout(resolve, ms);
     } else {
-      reject(console.log('Shop is closed'));
+      reject(console.log('Shop is closed.'));
     }
   });
 }
 
-order(2000, () => console.log(`${stock.fruits[0]} was chosen.`))
-  .then(() => order(0, () => console.log('Production has started')))
-  .then(() => order(2000, () => console.log(`The fruit was chopped.`)))
-  .then(() =>
-    order(1000, () => {
-      console.log(`${stock.liquid[0]} and ${stock.liquid[1]} were chosen.`);
-    })
-  )
-  .then(() => order(1000, () => console.log('Started the machine.')))
-  .then(() =>
-    order(2000, () => {
-      console.log(`Ice cream placed on ${stock.holder[0]}.`);
-    })
-  )
-  .then(() =>
-    order(3000, () => {
-      console.log(`${stock.toppings[0]} was selected.`);
-    })
-  )
-  .then(() =>
-    order(2000, () => {
-      console.log('Ice Cream was served.');
-    })
-  )
-  .catch(() => {
-    console.log('Customer left');
-  })
-  .finally(() => {
-    console.log('Day ended, shop closed');
-  });
+async function kitchen() {
+  try {
+    await time(2000);
+    console.log(`${stock.fruits[0]} was selected`);
+
+    await time(0);
+    console.log('Start the production.');
+
+    await time(2000);
+    console.log('cut the fruit');
+
+    await time(1000);
+    console.log(`${stock.liquid[0]} and ${stock.liquid[1]} were added.`);
+
+    await time(1000);
+    console.log('Starting machine.');
+
+    await time(2000);
+    console.log(`Ice cream placed on ${stock.holder[0]}.`);
+
+    await time(3000);
+    console.log(`${stock.toppings[0]} was selected.`);
+
+    await time(2000);
+    console.log('Served ice cream.');
+  } catch (error) {
+    console.log('Customer left.', error);
+  } finally {
+    console.log('Shop is closed for the day.');
+  }
+}
+
+kitchen();
