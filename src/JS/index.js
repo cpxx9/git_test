b25b98d12e88ec67ccac064c17c015ddcaa1f2e8
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-04-10 11:34:48 -0500

    wds callbacks and promises

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,54 +1,55 @@
 import '../CSS/styles.css';
 
-const stock = {
-  fruits: ['strawberry', 'grape', 'banana', 'apple'],
-  liquid: ['water', 'ice'],
-  holder: ['cone', 'cup', 'stick'],
-  toppings: ['chocolate', 'peanuts'],
-};
+const userLeft = true;
+const userWatchingCatMeme = false;
 
-const isShopOpen = false;
+// function watchTutorialCallback(callback, errorCallback) {
+//   if (userLeft) {
+//     errorCallback({
+//       name: 'User Left',
+//       message: ':(',
+//     });
+//   } else if (userWatchingCatMeme) {
+//     errorCallback({
+//       name: 'User Watching Cat Meme',
+//       message: 'WDS < Cat',
+//     });
+//   } else {
+//     callback('Thumbs up and Subscribe');
+//   }
+// }
 
-function time(ms) {
-  return new Promise((resolve, reject) => {
-    if (isShopOpen) {
-      setTimeout(resolve, ms);
-    } else {
-      reject(console.log('Shop is closed.'));
-    }
-  });
-}
+// watchTutorialCallback(
+//   (message) => {
+//     console.log(`Success: ${message}`);
+//   },
+//   (error) => {
+//     console.log(`${error.name}: ${error.message}`);
+//   }
+// );
 
-async function kitchen() {
-  try {
-    await time(2000);
-    console.log(`${stock.fruits[0]} was selected`);
+// function watchTutorialPromise() {
+//   return new Promise((resolve, reject) => {
+//     if (userLeft) {
+//       reject({
+//         name: 'User Left',
+//         message: ':(',
+//       });
+//     } else if (userWatchingCatMeme) {
+//       reject({
+//         name: 'User Watching Cat Meme',
+//         message: 'WDS < Cat',
+//       });
+//     } else {
+//       resolve('Thumbs up and Subscribe');
+//     }
+//   });
+// }
 
-    await time(0);
-    console.log('Start the production.');
-
-    await time(2000);
-    console.log('cut the fruit');
-
-    await time(1000);
-    console.log(`${stock.liquid[0]} and ${stock.liquid[1]} were added.`);
-
-    await time(1000);
-    console.log('Starting machine.');
-
-    await time(2000);
-    console.log(`Ice cream placed on ${stock.holder[0]}.`);
-
-    await time(3000);
-    console.log(`${stock.toppings[0]} was selected.`);
-
-    await time(2000);
-    console.log('Served ice cream.');
-  } catch (error) {
-    console.log('Customer left.', error);
-  } finally {
-    console.log('Shop is closed for the day.');
-  }
-}
-
-kitchen();
+// watchTutorialPromise()
+//   .then((message) => {
+//     console.log(`Success: ${message}`);
+//   })
+//   .catch((error) => {
+//     console.log(`${error.name}: ${error.message}`);
+//   });
