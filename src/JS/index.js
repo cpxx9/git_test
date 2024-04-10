4c88a1bd353ca4bfb4822bd90ee410f31decd784
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-04-10 16:54:22 -0500

    wds async

--- src/JS/index.js
+++ src/JS/index.js
@@ -71,3 +71,45 @@ import '../CSS/styles.css';
 // Promise.race(promiseArr).then((message) => {
 //   console.log(message);
 // });
+
+function makeRequest(location) {
+  return new Promise((resolve, reject) => {
+    console.log(`Making Request to ${location}`);
+    if (location === 'Google') {
+      resolve('Google says hi');
+    } else {
+      reject('We can only talk to Google');
+    }
+  });
+}
+
+function processRequest(response) {
+  return new Promise((resolve, reject) => {
+    console.log('Processing response');
+    resolve(`Extra information: ${response}`);
+  });
+}
+
+// makeRequest('Google')
+//   .then((response) => {
+//     console.log('Response has been received');
+//     return processRequest(response);
+//   })
+//   .then((processedResponse) => {
+//     console.log(processedResponse);
+//   }).catch(err => {
+//      console.log(err);
+//   });
+
+async function doWork() {
+  try {
+    const response = await makeRequest('google');
+    console.log('response received');
+    const processedResponse = await processRequest(response);
+    console.log(processedResponse);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+doWork();
