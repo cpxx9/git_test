aa69dfefd1ee41562e9204a94e0fc87d4623ccf0
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-04-11 10:58:53 -0500

    weather api async example

--- src/JS/index.js
+++ src/JS/index.js
@@ -38,32 +38,50 @@ import '../CSS/styles.css';
 
 // getWeather().then(getWeahterIcon).then(onSuccess).catch(onFail);
 
-function fun1() {
+// function fun1() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve('Good data');
+//     }, 200);
+//   });
+// }
+
+// function fun2() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve('🥸');
+//     }, 500);
+//   });
+// }
+
+// function onSuccess(data) {
+//   console.log(`Success: ${data}`);
+// }
+
+// function onError(errorCode) {
+//   console.log(`Error: ${errorCode}`);
+// }
+
+// function inTheEnd() {
+//   console.log('We be done');
+// }
+
+// fun1().then(fun2).then(onSuccess).catch(onError).finally(inTheEnd);
+
+function fetchData() {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('Good data');
-    }, 200);
+    fetch('https://api.weather.gov/gridpoints/OKX/35,35/forecast')
+      .then((response) => response.json())
+      .then((data) => resolve(data.properties.periods[1].shortForecast));
   });
 }
 
-function fun2() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('🥸');
-    }, 500);
-  });
+function displayData(weather) {
+  console.log(weather);
 }
 
-function onSuccess(data) {
-  console.log(`Success: ${data}`);
+function onError(err) {
+  console.log(`Error: ${err}`);
 }
 
-function onError(errorCode) {
-  console.log(`Error: ${errorCode}`);
-}
-
-function inTheEnd() {
-  console.log('We be done');
-}
-
-fun1().then(fun2).then(onSuccess).catch(onError).finally(inTheEnd);
+fetchData().then(displayData).catch(onError);
