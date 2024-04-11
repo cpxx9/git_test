32f22dd84e7249bf1898414b7fb7888e73d68c7e
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-04-11 09:19:26 -0500

    ColorCode promises

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,115 +1,39 @@
 import '../CSS/styles.css';
 
-// const userLeft = false;
-// const userWatchingCatMeme = false;
-
-// function watchTutorialCallback(callback, errorCallback) {
-//   if (userLeft) {
-//     errorCallback({
-//       name: 'User Left',
-//       message: ':(',
-//     });
-//   } else if (userWatchingCatMeme) {
-//     errorCallback({
-//       name: 'User Watching Cat Meme',
-//       message: 'WDS < Cat',
-//     });
-//   } else {
-//     callback('Thumbs up and Subscribe');
-//   }
-// }
-
-// watchTutorialCallback(
-//   (message) => {
-//     console.log(`Success: ${message}`);
-//   },
-//   (error) => {
-//     console.log(`${error.name}: ${error.message}`);
-//   }
-// );
-
-// function watchTutorialPromise() {
-//   return new Promise((resolve, reject) => {
-//     if (userLeft) {
-//       reject({
-//         name: 'User Left',
-//         message: ':(',
-//       });
-//     } else if (userWatchingCatMeme) {
-//       reject({
-//         name: 'User Watching Cat Meme',
-//         message: 'WDS < Cat',
-//       });
-//     } else {
-//       resolve('Thumbs up and Subscribe');
-//     }
-//   });
-// }
-
-// watchTutorialPromise()
-//   .then((message) => {
-//     console.log(`Success: ${message}`);
-//   })
-//   .catch((error) => {
-//     console.log(`${error.name}: ${error.message}`);
-//   });
-
-// const recordVideoOne = new Promise((resolve, reject) => {
-//   resolve('Video 1 Recorded');
-// });
-
-// const recordVideoTwo = new Promise((resolve, reject) => {
-//   resolve('Video 2 Recorded');
-// });
-
-// const recordVideoThree = new Promise((resolve, reject) => {
-//   resolve('Video 3 Recorded');
-// });
-
-// const promiseArr = [recordVideoOne, recordVideoTwo, recordVideoThree];
-
-// Promise.race(promiseArr).then((message) => {
-//   console.log(message);
-// });
-
-function makeRequest(location) {
+function getWeather() {
   return new Promise((resolve, reject) => {
-    console.log(`Making Request to ${location}`);
-    if (location === 'Google') {
-      resolve('Google says hi');
-    } else {
-      reject('We can only talk to Google');
-    }
+    setTimeout(() => {
+      resolve('cloudy');
+    }, 500);
   });
 }
 
-function processRequest(response) {
+function getWeahterIcon(weather) {
   return new Promise((resolve, reject) => {
-    console.log('Processing response');
-    resolve(`Extra information: ${response}`);
+    setTimeout(() => {
+      switch (weather) {
+        case 'Sunny':
+          resolve('☀️');
+          break;
+        case 'Cloudy':
+          resolve('☁️');
+          break;
+        case 'Raining':
+          resolve('🌧️');
+          break;
+        default:
+          reject('NO ICON FOUND');
+      }
+    }, 1000);
   });
 }
 
-// makeRequest('Google')
-//   .then((response) => {
-//     console.log('Response has been received');
-//     return processRequest(response);
-//   })
-//   .then((processedResponse) => {
-//     console.log(processedResponse);
-//   }).catch(err => {
-//      console.log(err);
-//   });
+const onSuccess = (data) => {
+  console.log(`Success: ${data}`);
+};
 
-async function doWork() {
-  try {
-    const response = await makeRequest('google');
-    console.log('response received');
-    const processedResponse = await processRequest(response);
-    console.log(processedResponse);
-  } catch (err) {
-    console.log(err);
-  }
-}
+const onFail = (error) => {
+  console.log(`Error: ${error}`);
+};
 
-doWork();
+getWeather().then(getWeahterIcon).then(onSuccess).catch(onFail);
