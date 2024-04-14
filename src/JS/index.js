122bcb7b92edf41f5050dfb7d01422afc36fb0fb
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-04-13 20:04:38 -0500

    js.info async/await

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,22 +1,11 @@
 import '../CSS/styles.css';
 
-const img = document.querySelector('img');
-const changeGifBtn = document.querySelector('#changeGif');
-const searchBox = document.querySelector('#search');
-
-async function getGif(searchTerm) {
-  const response = await fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=niJ4T4l1OG8uS68mGo6Qr5m5nkvVbOLc&s=${searchTerm}`,
-    {
-      mode: 'cors',
-    }
+async function showAvatar() {
+  const user = { name: 'cpxx9' };
+  const githubResponse = await fetch(
+    `https://api.github.com/users/${user.name}`
   );
-  const apiSrc = await response.json();
-  img.src = apiSrc.data.images.original.url;
+  const githubUser = githubResponse.json();
 }
 
-getGif('cats');
-
-changeGifBtn.addEventListener('click', () => {
-  getGif(searchBox.value);
-});
+showAvatar();
