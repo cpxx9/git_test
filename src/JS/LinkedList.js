fbf954f3b49da8f304a115067fe0cdf6f320575e
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-05-01 13:06:04 -0400

    adding nodes logic

--- src/JS/LinkedList.js
+++ src/JS/LinkedList.js
@@ -8,6 +8,63 @@ class LinkedList {
 
   insertFirst(data) {
     this.head = new Node(data, this.head);
+    this.size += 1;
+  }
+
+  insertLast(data) {
+    const node = new Node(data);
+    let current;
+    if (!this.head) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.size += 1;
+  }
+
+  insertAt(data, index) {
+    if (index < 0) {
+      return;
+    }
+
+    if (index === 0) {
+      this.insertFirst(data);
+      return;
+    }
+
+    if (index >= this.size) {
+      this.insertLast(data);
+      return;
+    }
+
+    const node = new Node(data);
+    let curr;
+    let prev;
+    curr = this.head;
+    let count = 0;
+
+    while (count < index) {
+      prev = curr;
+      count += 1;
+      curr = curr.next;
+    }
+
+    node.next = curr;
+    prev.next = node;
+
+    this.size += 1;
+  }
+
+  printListData() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
   }
 }
 
--- src/JS/index.js
+++ src/JS/index.js
@@ -4,5 +4,9 @@ import LinkedList from './LinkedList';
 
 const ll = new LinkedList();
 ll.insertFirst(100);
+ll.insertFirst(200);
+ll.insertFirst(300);
+ll.insertLast(400);
+ll.insertAt(250, 0);
 
-console.log(ll);
+ll.printListData();
