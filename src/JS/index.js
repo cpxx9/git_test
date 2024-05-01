6f222bd5caa211a7080819a2e034e74f55d9e8c7
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-05-01 09:49:45 -0400

    babel working with npm

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
-import '../CSS/styles.css';
+// import '../CSS/styles.css';
+import { add, subtract } from './module.js';
+
+console.log(add(7, 6));
+console.log(subtract(7, 6));
