b62e389ef4b006a52a642d416bbd59767e8b6e6a
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-05-15 15:03:05 -0400

    Change: removed old tests

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,2 +1,10 @@
 #!/usr/bin/env node
 // import '../CSS/styles.css';
+function orderTotal(order) {
+  return order.items.reduce(
+    (prev, curr) => curr.price * (curr.quantity || 1) + prev,
+    0
+  );
+}
+
+export default orderTotal;
