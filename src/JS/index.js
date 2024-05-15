21f00487de35501b8179d6b8d277d8e8962518bb
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-05-15 09:23:27 -0400

    change: removed unneeded files and code to start new section

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,13 +1,2 @@
 #!/usr/bin/env node
 // import '../CSS/styles.css';
-import LinkedList from './LinkedList';
-
-const ll = new LinkedList();
-ll.insertFirst(100);
-ll.insertFirst(200);
-ll.insertFirst(300);
-ll.insertLast(400);
-
-ll.clearList();
-console.log(ll);
-// ll.getAt(2);
