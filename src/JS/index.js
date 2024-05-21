660b10df943f534b0737e867cf360e54afbfac8a
CPxx9 <127376939+cpxx9@users.noreply.github.com>
2024-05-21 19:00:32 -0400

    prettier config change

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 // import '../CSS/styles.css';
-function fetchData(callback) {
-  setTimeout(() => {
-    callback('peanut butter');
-  }, 1000);
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve('peanut butter'), 1000);
+  });
 }
 
+console.log('test');
 export default fetchData;
--- src/JS/index.test.js
+++ src/JS/index.test.js
@@ -1,1 +1,14 @@
 import fetchData from '.';
+
+test('fetched peanut butter', (done) => {
+  function callback(data) {
+    try {
+      expect(data).toBe('peanut butter');
+      done();
+    } catch (error) {
+      done(error);
+    }
+  }
+
+  fetchData(callback);
+});
