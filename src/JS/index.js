93dbfac2d95ed1fc95dcffb870b3b7004da9589c
CPxx9 <127376939+cpxx9@users.noreply.github.com>
2024-05-21 16:54:04 -0400

    mock test testing

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,17 +1,9 @@
 #!/usr/bin/env node
 // import '../CSS/styles.css';
-function orderTotal(fetch, order) {
-  fetch(`https://jsonplaceholder.typicode.com/users/${order.indexCode}/todos`);
-  return Promise.resolve(
-    order.items.reduce(
-      (prev, curr) => curr.price * (curr.quantity || 1) + prev,
-      0
-    )
-  );
+function fetchData(callback) {
+  setTimeout(() => {
+    callback('peanut butter');
+  }, 1000);
 }
 
-// const result = fetch('https://jsonplaceholder.typicode.com/users/1/todos')
-//   .then((response) => response.json())
-//   .then((data) => console.log(data[0].title.length));
-
-export default orderTotal;
+export default fetchData;
