a99c8e925ad7ed90c1ac566e5bf5ea1fb67629c2
CPxx9 <127376939+cpxx9@users.noreply.github.com>
2024-06-24 16:09:23 -0400

    mdn transitions

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,1 +1,11 @@
 import '../CSS/styles.css';
+
+const divElm = document.querySelector('div');
+const htmlElm = document.querySelector(':root');
+
+htmlElm.addEventListener('click', showHide);
+document.addEventListener('keydown', showHide);
+
+function showHide() {
+  divElm.classList.toggle('showing');
+}
