66367a47dcb401676d1078823586674e92fd1309
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-06-27 13:22:19 -0400

    animation performance

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,11 +1,1 @@
 import '../CSS/styles.css';
-
-const divElm = document.querySelector('div');
-const htmlElm = document.querySelector(':root');
-
-htmlElm.addEventListener('click', showHide);
-document.addEventListener('keydown', showHide);
-
-function showHide() {
-  divElm.classList.toggle('showing');
-}
